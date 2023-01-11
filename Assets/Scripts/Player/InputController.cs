beea275b6cff64cc35ba5d42fba3f7bfd8a2b4fe
UmutCanT <uctekcambusiness@gmail.com>
2023-01-11 18:12:41 +0300

    Smooth Tile to Tile Movement
    Inputs and movements at seperate classes.
    (Different from the video)

---
+++ Assets/Scripts/Player/InputController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    private Vector2 playerInput;
+
+    public Vector2 PlayerInput { get => playerInput; private set => playerInput = value; }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerInput.x = Input.GetAxisRaw("Horizontal");
+        playerInput.y = Input.GetAxisRaw("Vertical"); 
+    }
+}
