46ae6b5f6fef47e58cc34c1c558669d913c93793
UmutCanT <uctekcambusiness@gmail.com>
2023-01-15 10:30:58 +0300

    Moves Template and Moveset for Pokemon Implemented

---
+++ Assets/Scripts/Pokemon/LearnableMoveset.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class LearnableMoveset
+{
+    [SerializeField] int level;
+    [SerializeField] MoveTemplate moveTemplete;
+
+    public int Level { get => level; }
+    public MoveTemplate MoveTemplete { get => moveTemplete; }
+}
